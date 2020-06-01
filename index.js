6a582f6ff1d02690cc1c039d543d67879e73ee9e
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-06-01 13:43:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => console.log(json));
 
+.then(renderBooks())
 }
 
 function renderBooks(json) {
@@ -8,9 +12,12 @@ function renderBooks(json) {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
+
   })
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
+
 })
